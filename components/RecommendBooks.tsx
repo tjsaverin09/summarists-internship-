ff73d0b25c2080efea975c9312c392d605263742
Terence <tjsaverin98@live.com>
2026-02-03 06:19:38 -0600

    looking for best slider

--- components/RecommendBooks.tsx
+++ components/RecommendBooks.tsx
@@ -1,53 +1,59 @@
-'use client'
+"use client";
 
+import React, { useEffect, useRef } from "react";
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import { useGetRecBooksQuery } from "@/redux/recBooksApiSlice";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import Link from "next/link";
 
-
 export default function RecommendedBooks() {
-    const { data: recBooksArray} = useGetRecBooksQuery()
+  const { data: recBooksArray } = useGetRecBooksQuery();
+  const swiperRef = useRef<Swiper | null>(null);
 
-  function MultipleItems() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 3
-    }
-  }
+  useEffect(() => {
+if (!recBooksArray || recBooksArray.length === 0) return;
 
-    return (
-        <div className="recBooks">
-            <div className="foryou__header">Recommended For You</div>
-            <div className="foryou__subtitle">We think you'll like these</div>
-            <div  className="bookList slider-container">
-              {recBooksArray?.map((books) => (
-                <Slider {...settings}>
-                <Link href={`book/${books?.id || "no-id"}`} key={books.id}>
-                <div  className="book">
+    swiperRef.current = new Swiper(".swiper", {
+      modules: [Navigation, Pagination],
+      direction: "horizontal",
+    });
+
+    return () => {
+      swiperRef.current?.destroy();
+    };
+  }, [recBooksArray]);
+
+  return (
+    <div className="recBooks">
+      <div className="foryou__header">Recommended For You</div>
+      <div className="foryou__subtitle">We think you'll like these</div>
+      <div className="bookList swiper">
+        {recBooksArray?.map((books) => (
+          <div className="swiper-wrapper">
+            <Link href={`book/${books?.id || "no-id"}`} key={books.id}>
+              <div className="book swiper-slide">
                 <div className="bookImageWrapper">
-                  <img src={books.imageLink} alt={books.title} className="bookImage" />
+                  <img
+                    src={books.imageLink}
+                    alt={books.title}
+                    className="bookImage"
+                  />
                 </div>
-                <div className="bookName">
-                  {books.title}
-                </div>
+                <div className="bookName">{books.title}</div>
                 <div className="bookAuthor">{books.author}</div>
-                <div className="bookDescription">
-                  {books.subTitle}
-                </div>
+                <div className="bookDescription">{books.subTitle}</div>
                 <div className="extraDetails">
                   <div className="bookLength"></div>
                   <div className="bookRating">{books.averageRating}</div>
                 </div>
               </div>
-              </Link>
-              </Slider>
-              ))}
-              
-            </div>
+            </Link>
           </div>
-    )
+        ))}
+      </div>
+    </div>
+  );
 }
